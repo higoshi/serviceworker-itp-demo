130dd5b970de0fce544e4ec7f640f4803e421232
yoji.higoshi <h33je5sg@gmail.com>
2018-11-08 18:27:13 +0900

    fixed

--- README.md
+++ README.md
@@ -9,12 +9,12 @@ $ sudo -i
 # echo '127.0.0.1       loopback.example.net' >> /etc/hosts
 # exit
 
-$ openssl req -new -x509 -sha256 -newkey rsa:2048 -days 365 -nodes -out ./proxy/cert/cert.crt -keyout ./proxy/cert/key.key
-$ openssl req -new -x509 -sha256 -newkey rsa:2048 -days 365 -nodes -out ./first/cert/cert.crt -keyout ./first/cert/key.key
+$ openssl req -new -x509 -sha256 -newkey rsa:2048 -days 365 -nodes -out ./proxy/cert/crt.crt -keyout ./proxy/cert/key.key
+$ openssl req -new -x509 -sha256 -newkey rsa:2048 -days 365 -nodes -out ./first/cert/crt.crt -keyout ./first/cert/key.key
 ```
 
 ## Start
 ```sh
 $ sudo docker-compose up -d
-$ open https://loopback.example.net:4434/
+$ open https://loopback.example.net:4433/
 ```
--- app/html/index.php
+++ app/html/index.php
@@ -1,24 +1,27 @@
 <!doctype html>
-<body>
-  <script>
-    void async function() {
-      if (!'serviceWorker' in navigator) throw 'Not supported ServiceWorker';
-      const {serviceWorker} = navigator;
-      const registration = await serviceWorker.register('./sw.js').catch((error) => {
-        console.log(error);
-      });
-      // console.log(registration)
-      // if (registration) registration.unregister(registration);
+<ul>
+  <li id="referrer"></li>
+  <li id="date"></li>>
+</ul>
+<script>
+  void async function() {
+    document.getElementById('referrer').innerText = document.referrer;
+    if (!'serviceWorker' in navigator) throw 'Not supported ServiceWorker';
+    const {serviceWorker} = navigator;
+    const registration = await serviceWorker.register('./sw.js').catch((error) => {
+      console.log(error);
+    });
+    // console.log(registration)
+    // if (registration) registration.unregister(registration);
 
-      serviceWorker.addEventListener('message', event => {
-        console.log(event);
-      });
+    serviceWorker.addEventListener('message', event => {
+      console.log(event);
+    });
 
-      setInterval(async () => {
-        const response = await fetch('api/getDate.php');
-        const text = await response.text();
-        document.write(text);
-      }, 1000);
-    }();
-  </script>
-</body>
+    setInterval(async () => {
+      const response = await fetch('api/getDate.php');
+      const text = await response.text();
+      document.getElementById('date').innerText = text;
+    }, 1000);
+  }();
+</script>
